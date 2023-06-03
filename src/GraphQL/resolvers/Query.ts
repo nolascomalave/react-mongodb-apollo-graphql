fdac17537cb380aa48c306e9c7a51fee8b904f0e
Ocam <=>
2023-06-03 11:02:21 -0400

    Primer commit del repositorio del curso de React con Next, Apollo, GraphQL y MongoDB.

---
+++ src/GraphQL/resolvers/Query.ts
@@ -0,0 +1,13 @@
+const cursos = [
+    {
+        titulo: 'Example 1',
+        tecnologia: 'Example Tecnology'
+    },
+    {
+        titulo: 'Example 2',
+        tecnologia: 'Example Tecnology 2'
+    }
+];
+
+export const obtenerCursos = () => cursos;
+export const obtenerTecnologias = () => cursos;
---
+++ src/index.ts
@@ -0,0 +1,32 @@
+import fs from 'fs';
+import path from 'path';
+// import express from 'express';
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from '@apollo/server/standalone';
+
+// GraphQL Dependecies:
+import resolvers from './GraphQL/resolvers'; // Resolvers:
+const typeDefs = fs.readFileSync(path.join(__dirname, './GraphQL/schemas.gql'), 'utf-8'); // Schemas:
+
+// Servers:
+// const app = express();
+const apollo = new ApolloServer({
+    resolvers,
+    typeDefs
+});
+
+startStandaloneServer(apollo, {
+    listen: { port: 4000 },
+})
+    .then(({ url }) => {
+        console.log(`🚀  Server ready at: ${url}`);
+    });
+
+
+/* app.listen(0, async () => {
+    const { url } = await startStandaloneServer(apollo, {
+        listen: { port: 4000 },
+    });
+
+    console.log(`🚀  Server ready at: ${url}`);
+}); */
